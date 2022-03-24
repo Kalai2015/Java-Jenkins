0d61b0608a9d795942c4bc87c4ca7794909278cd
kalai <kalaiganeshan@gmail.com>
2022-03-24 04:56:14 -0400

    Updated linked list

--- src/main/java/com/springdemo/testMaven/App.java
+++ src/main/java/com/springdemo/testMaven/App.java
@@ -1,5 +1,8 @@
 package com.springdemo.testMaven;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World using Docker with second commit " );
+        
+        Integer[] arr =  {2,4,5,1,8};
+        List<Integer> list = Arrays.asList(arr);
+        
+        System.out.println("List content : "+list);
     }
 }
---
+++ src/main/java/com/springdemo/testMaven/SingleLinkedList.java
@@ -0,0 +1,53 @@
+package com.springdemo.testMaven;
+
+class ListNode {
+	int data;
+	ListNode nextNode;
+
+}
+
+public class SingleLinkedList {
+
+	private ListNode head;
+
+	public void insert(int data) {
+
+		ListNode newNode = new ListNode();
+		newNode.data = data;
+		newNode.nextNode = head;
+		head = newNode;
+	}
+	
+	public void delete()
+	{
+		head = head.nextNode;
+	}
+	
+	public void printList()
+	{
+		System.out.println("Inside printlist");
+		ListNode current = head;
+		while(current != null)
+		{
+			System.out.print(current.data +"-->");
+			current = current.nextNode;
+		}
+		
+		System.out.println("Null");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SingleLinkedList list = new SingleLinkedList();
+		
+		list.insert(10);
+		list.insert(20);
+		list.insert(30);
+		list.insert(40);
+		list.insert(50);
+
+        list.printList();
+	}
+
+}
